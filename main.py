7ff6f31b51c52efd2479f2c8709ae688378d9323
Ilyass <ilyass9472@gmail.com>
2026-07-20 11:36:07 +0100

    ajout first version dechifrement and creat db with tables

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from modules.chiffreur import chiffrement
+from modules.dechiffreur import dechiffreur
 from utils.input import Input
 from utils.cle import cle
+from data.config import get_connection
+try:
+    conn = get_connection()
+    print("Connexion réussie !")
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+
+    print(cursor.fetchone())
+
+    cursor.close()
+    conn.close()
+
+except Exception as e:
+    print("Erreur :", e)
 reader = Input("")
 text = reader.get_text()
 key = cle()
 
-print(chiffrement(text, key))
+switcher = ""
+
+while switcher not in ["C", "D"]:
+    switcher = input("Voulez-vous chiffrer ou déchiffrer le texte ? (C/D) : ").upper()
+
+if switcher == "C":
+    print(chiffrement(text, key))
+
+elif switcher == "D":
+    dechiffreur_obj = dechiffreur(text, key)
+    print(dechiffreur_obj.dechiffrement())
---
+++ modules/dechiffreur.py
@@ -0,0 +1,77 @@
+class dechiffreur:
+    def __init__(self, texte_chiffre, cle):
+        self.texte_chiffre = texte_chiffre
+        self.cle = cle
+
+    def dechiffrement(self):
+
+        letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+                   "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+        Letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+                   "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+        numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+        new_list_letters = []
+        New_list_letters = []
+        new_list_numbers = []
+        new_letters = []
+
+        cle = self.cle % 26
+
+        # Construction du texte
+        for i in range(len(self.texte_chiffre)):
+            new_letters.append(self.texte_chiffre[i])
+
+        # Alphabet minuscule et majuscule
+        i = cle
+        while i < len(letters):
+            new_list_letters.append(letters[i])
+            New_list_letters.append(Letters[i])
+
+            if i == len(letters) - 1:
+                i = -1
+
+            if i == cle - 1:
+                break
+
+            i += 1
+
+        # Nombres
+        i = cle % 10
+        while i < len(numbers):
+            new_list_numbers.append(numbers[i])
+
+            if i == len(numbers) - 1:
+                i = -1
+
+            if i == (cle % 10) - 1:
+                break
+
+            i += 1
+
+        # Déchiffrement
+        i = 0
+        while i < len(new_letters):
+
+            if new_letters[i] in new_list_letters:
+                index = new_list_letters.index(new_letters[i])
+                new_letters[i] = letters[index]
+
+            elif new_letters[i] in New_list_letters:
+                index = New_list_letters.index(new_letters[i])
+                new_letters[i] = Letters[index]
+
+            elif new_letters[i] in new_list_numbers:
+                index = new_list_numbers.index(new_letters[i])
+                new_letters[i] = numbers[index]
+
+            i += 1
+
+        texte_dechiffre = ""
+
+        for i in range(len(new_letters)):
+            texte_dechiffre += new_letters[i]
+
+        return texte_dechiffre
