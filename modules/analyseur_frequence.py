e09f87398a32ca0ce74be4fe3b8c9a3a613f08c4
Ilyass <ilyass9472@gmail.com>
2026-07-28 11:13:10 +0100

    analyse frequence text chifrée

--- modules/analyseur_frequence.py
+++ modules/analyseur_frequence.py
@@ -1,22 +1,86 @@
+
+from data.config import get_connection
+from collections import Counter
+
+
 class AnalyseurFrequence:
     def __init__(self, texte):
         self.texte = texte
         self.frequences = 0
     def calculer_frequence(self):
-            caracteres = [
-            c for c in self.texte
-            if c.islower() or c.isupper() or c.isdigit()
-            or (not c.isalnum() and not c.isspace())
-            ]
-            i=0
+        try:
+            conn = get_connection()
+        except Exception as e:
+            print("Erreur lors de la connexion à la base de données :", e)
+        cursor = conn.cursor()
+        cursor.execute("UPDATE lettres_lower_chiffre SET frequence = 0")
+        cursor.execute("UPDATE lettres_upper_chiffre SET frequence = 0")
+        cursor.execute("UPDATE nombres_chiffre SET frequence = 0")
+        cursor.execute("UPDATE symbole_chiffre SET frequence = 0")
+        print("Longueur du texte :", len(self.texte))
+    
+        caracteres = [
+        c for c in self.texte
+        if c.islower() or c.isupper() or c.isdigit()
+        or (not c.isalnum() and not c.isspace())
+        ]
+        i=0
             
-            while i < len(self.texte):
-                j=0
-                while j < len(self.texte):
-                     if self.texte[i] == self.texte[j]:
-
-                        if self.texte[i].islower() and self.texte[i] in lettres_lower:
-                             self.frequences
-                            
-                j += 1
-            i += 1
+        cursor.execute("""
+            SELECT current_database(),
+               current_user,
+               inet_server_addr(),
+               inet_server_port();
+            """)
+        
+        print(cursor.fetchone())
+        total = len(caracteres)
+        cursor.execute("""
+            SELECT table_schema, table_name
+            FROM information_schema.tables
+            WHERE table_name='lettres_lower';
+            """)
+        
+        print(cursor.fetchall())
+        if total == 0:
+            print("Aucun caractère valide trouvé.")
+            cursor.close()
+            conn.close()
+            return
+        compteur = Counter(caracteres)
+        for caractere, repetition in compteur.items():
+            frequence = repetition / total
+            if caractere.islower():
+                try:
+                    cursor.execute(
+                    """
+                    UPDATE lettres_lower_chiffre
+                    SET frequence = %s
+                    WHERE lettre = %s
+                    """,
+                    (frequence, caractere)
+                )
+                except Exception as e:
+                    print("Erreur lors de la mise à jour de la fréquence des lettres minuscules :", e)
+            elif caractere.isupper():
+                try:
+                    
+                    cursor.execute(""" UPDATE lettres_upper_chiffre SET frequence = %s WHERE lettre = %s  """, (frequence, caractere))
+                except Exception as e:
+                    print("Erreur lors de la mise à jour de la fréquence des lettres majuscules :", e)
+            elif caractere.isdigit():
+                try:
+                    cursor.execute(""" UPDATE nombres_chiffre SET frequence = %s WHERE chiffre = %s  """, (frequence, caractere))
+                except Exception as e:
+                    print("Erreur lors de la mise à jour de la fréquence des nombres :",e)
+            elif not caractere.isalnum() and not caractere.isspace():
+                try:
+                    cursor.execute(""" UPDATE symbole_chiffre SET frequence = %s WHERE symbole = %s  """, (frequence, caractere))
+                except Exception as e:
+                    print("Erreur lors de la mise à jour de la fréquence des symboles :", e)
+            conn.commit()
+        cursor.execute("SELECT lettre, frequence FROM lettres_lower ORDER BY lettre;")
+        print(cursor.fetchall(  ))
+        print("Fréquences de text chiffré calculées avec succès.")
+        cursor.close()
+        conn.close()
